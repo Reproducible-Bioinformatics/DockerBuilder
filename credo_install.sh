3b15c560f1d69e413e37d088857b8a0ecae65860
Luca Alessandri <alessandri.luca91@gmail.com>
2025-07-02 11:04:14 +0200

    Update credo_install.sh

--- credo_install.sh
+++ credo_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+apt-get update
 echo "📁 Creating and moving to /credo_env..."
 mkdir -p /credo_env && cd /credo_env
 
@@ -11,5 +11,6 @@ credo pip numpy
 credo pip scikit-learn
 credo pip tensorflow
 credo pip jupyterlab
-
+credo bioconductor GenomicRanges
 echo "✅ Done with credo pip commands."
+credo save
