34b7924fcbc2597b5219989f893f46812bd9ebd6
Gianluca <gianluca@pop-os.localdomain>
2025-07-18 16:02:25 +0200

    update

--- credo_install.sh
+++ credo_install.sh
@@ -5,12 +5,13 @@ echo "📁 Creating and moving to /credo_env..."
 mkdir -p /credo_env && cd /credo_env
 
 echo "📦 Installing packages with CREDO pip..."
-
+credo pip jupyterlab
+credo pip seaborn
 credo pip pandas
 credo pip numpy
 credo pip scikit-learn
 credo pip tensorflow
-credo pip jupyterlab
-credo bioconductor GenomicRanges
+credo pip matplotlib
+credo pip umap-learn
 echo "✅ Done with credo pip commands."
 credo save
